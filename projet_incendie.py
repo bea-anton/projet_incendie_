59fe3b8253e2cf5d269702c144eb3d14c7c71f80
Beatriz Anton <beatriz.anton-artaza@ens.uvsq.fr>
2021-02-11 09:49:34 +0100

    projet py

---
+++ projet_incendie.py
@@ -0,0 +1,35 @@
+#############################################
+#groupe MIASHS 1
+#Alae KARTOUT
+#Basma BAGNAH AMADOU
+#Beatriz ANTON ARTAZA
+# https://github.com/bea-anton/projet_incendie.git
+##############################################
+
+##############################################
+# import des librairies
+
+import tkinter as tkinter
+
+#############################################
+#constantes
+
+COULEUR_FOND = "gray"
+LARGEUR = 
+HAUTEUR = 
+
+##############################################
+#fonctions 
+
+
+##############################################
+#programme principal 
+
+racine = tk.Tk()
+racine.title("Projet incendie")
+#creation des widgets
+canvas = tk.Canvas(racine, bg=COULEUR_FOND, width = LARGEUR, height = HAUTEUR)
+#PLACEMENT DES WIDGETS
+canvas.grid(row=0)
+#boucle principal
+racine.mainloop()
